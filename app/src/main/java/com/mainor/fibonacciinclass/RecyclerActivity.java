4624d43eaecf865f0872b411cddfb505df75dbb8
Dm <hello@ausalt.com>
2019-10-28 18:37:14 +0200

    5th commit

--- app/src/main/java/com/mainor/fibonacciinclass/RecyclerActivity.java
+++ app/src/main/java/com/mainor/fibonacciinclass/RecyclerActivity.java
@@ -1,6 +1,7 @@
 package com.mainor.fibonacciinclass;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
@@ -10,6 +11,6 @@ public class RecyclerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recycler);
-
+        RecyclerView recyclerView = findViewById(R.id.recycler);
     }
 }
