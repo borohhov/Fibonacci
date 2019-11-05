9b0a3119ff2c9f767d3d6514d2868da94005cff2
Dm <hello@ausalt.com>
2019-11-05 11:51:12 +0200

    Last commit

--- app/src/main/java/com/mainor/fibonacciinclass/FibonacciAdapter.java
+++ app/src/main/java/com/mainor/fibonacciinclass/FibonacciAdapter.java
@@ -23,7 +23,7 @@ public class FibonacciAdapter extends RecyclerView.Adapter<FibonacciAdapter.Fibo
     @NonNull
     @Override
     public FibonacciViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        LayoutInflater inflater = LayoutInflater.from(context);
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
         View view = inflater.inflate(R.layout.fib_data_layout, parent, false);
         return new FibonacciAdapter.FibonacciViewHolder(view);
     }
--- app/src/main/java/com/mainor/fibonacciinclass/RecyclerActivity.java
+++ app/src/main/java/com/mainor/fibonacciinclass/RecyclerActivity.java
@@ -1,18 +1,21 @@
 package com.mainor.fibonacciinclass;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class RecyclerActivity extends AppCompatActivity {
-
+    private RecyclerView.LayoutManager layoutManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recycler);
         RecyclerView recyclerView = findViewById(R.id.recycler);
+        layoutManager = new LinearLayoutManager(this);
         int count = getIntent().getIntExtra("Input", 10);
+        recyclerView.setLayoutManager(layoutManager);
         FibonacciAdapter adapter = new FibonacciAdapter(FibonacciGenerator.getFibonacciNums(count));
         recyclerView.setAdapter(adapter);
     }
